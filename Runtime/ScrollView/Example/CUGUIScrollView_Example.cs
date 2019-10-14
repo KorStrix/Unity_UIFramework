54674accae7262b0f15fb3a4ab40b9c0aea1830e
Strix <KorStrix@users.noreply.github.com>
2019-10-14 11:19:51 +0900

    스크롤뷰 커밋

---
+++ Runtime/ScrollView/Example/CUGUIScrollView_Example.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SG
+{
+    [RequireComponent(typeof(CUGUIScrollView))]
+    [DisallowMultipleComponent]
+    public class CUGUIScrollView_Example : MonoBehaviour
+    {
+        public int totalCount = -1;
+        void Start()
+        {
+            var ls = GetComponent<CUGUIScrollView>();
+            ls.DoUpdateScroll(totalCount);
+        }
+    }
+}
---
+++ Runtime/ScrollView/Example/ScrollItem_Example3.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScrollItem_Example3 : MonoBehaviour, IScrollItem
+{
+    public Text text;
+    public void IScrollItem_Update(int idx)
+    {
+        string name = "Cell " + idx.ToString();
+        if (text != null)
+        {
+            text.text = name;
+        }
+    }
+}

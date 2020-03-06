8bafb1eda7d277d9ed24f606849db5afb0409859
Strix <KorStrix@users.noreply.github.com>
2020-03-06 18:02:45 +0900

    no message

---
+++ Runtime/02.UIAnimation/UIObjectAnimatorComponent.cs
@@ -0,0 +1,50 @@
+#region Header
+/*	============================================
+ *	Aurthor 			    : Strix
+ *	Initial Creation Date 	: 2020-03-06
+ *	Summary 		        : 
+ *  Template 		        : For Unity Editor V1
+   ============================================ */
+#endregion Header
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UIFramework
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class UIObjectAnimatorComponent : UIObjectAnimatorComponent_UnManaged, IUIWidget_Managed
+    {
+        /* const & readonly declaration             */
+
+        /* enum & struct declaration                */
+
+        /* public - Field declaration               */
+
+
+        /* protected & private - Field declaration  */
+
+
+        // ========================================================================== //
+
+        /* public - [Do~Somthing] Function 	        */
+
+
+        // ========================================================================== //
+
+        /* protected - [Override & Unity API]       */
+
+
+        /* protected - [abstract & virtual]         */
+
+
+        // ========================================================================== //
+
+        #region Private
+
+        #endregion Private
+    }
+}
